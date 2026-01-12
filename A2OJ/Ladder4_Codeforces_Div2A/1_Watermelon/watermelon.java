b4150ab323b996a59a69ccb193b46db8c62438c3
Frederico S. Gonçalves <fredericosantos0309@gmail.com>
2026-01-12 18:48:13 -0300

    Update watermelon.java

--- A2OJ/Ladder4_Codeforces_Div2A/1_Watermelon/watermelon.java
+++ A2OJ/Ladder4_Codeforces_Div2A/1_Watermelon/watermelon.java
@@ -1,1 +1,21 @@
+/*
+ * Autor: Frederico Santos Gonçalves
+ * Data: 12/01/2026
+ *
+ * Plataforma: A2OJ
+ * Problema: Watermelon
+ * Link: https://codeforces.com/problemset/problem/4/A
+ * Dificuldade: 1
+ */
 
+import java.util.Scanner;
+
+public class watermelon {
+  static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+      int weight = scanner.nextInt();
+      if(weight != 2 && weight%2 == 0) System.out.print("YES");
+      else System.out.print("NO");
+  }
+}
