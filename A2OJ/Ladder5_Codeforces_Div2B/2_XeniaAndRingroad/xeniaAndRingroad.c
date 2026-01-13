4ce9e7adf250a8d41e812151791dfa9c96c0d8a9
Frederico S. Gonçalves <fredericosantos0309@gmail.com>
2026-01-13 12:03:50 -0300

    Update xeniaAndRingroad.c

--- A2OJ/Ladder5_Codeforces_Div2B/2_XeniaAndRingroad/xeniaAndRingroad.c
+++ A2OJ/Ladder5_Codeforces_Div2B/2_XeniaAndRingroad/xeniaAndRingroad.c
@@ -1,1 +1,32 @@
+/*
+ * Autor: Frederico Santos Gonçalves
+ * Data: 13/01/2026
+ *
+ * Plataforma: A2OJ
+ * Problema: Xenia and Ringroad
+ * Link: https://codeforces.com/problemset/problem/339/B
+ * Dificuldade: 2
+ */
 
+#include <stdio.h>
+#define SUCESSO 0
+
+int main() {
+  unsigned long long int n, m;
+  unsigned long long int pos = 1, nextPos, time = 0;
+
+  scanf("%llu %llu%*c", &n, &m);
+
+  for(int i = 0; i < m; i++) {
+    scanf("%llu", &nextPos);
+    if(nextPos >= pos) time += nextPos-pos;
+    else {
+      time += n-pos;
+      time += nextPos;
+    }
+    pos = nextPos;
+  }
+
+  printf("%llu", time);
+  return SUCESSO;
+}
