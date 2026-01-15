cf237a357d0d6462371f5faa4d81225146cecef4
Frederico S. Gonçalves <fredericosantos0309@gmail.com>
2026-01-15 20:50:16 -0300

    Create watermelon.cpp

---
+++ A2OJ/Ladder4_Codeforces_Div2A/1_Watermelon/watermelon.cpp
@@ -0,0 +1,23 @@
+/*
+ * Autor: Frederico Santos Gonçalves
+ * Data: 15/01/2026
+ *
+ * Plataforma: A2OJ
+ * Problema: Watermelon
+ * Link: https://codeforces.com/problemset/problem/4/A
+ * Dificuldade: 1
+ */
+
+#include <iostream>
+#define SUCESSO 0
+using namespace std;
+
+int main(void) {
+  int weight;
+  cin >> weight;
+
+  if(weight != 2 && weight%2 == 0) cout << "YES";
+  else cout << "NO";
+
+  return SUCESSO;
+}
