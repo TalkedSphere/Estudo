6ed2cabe087f8e3838c4ab16a10e645d95cc283c
Frederico S. Gonçalves <fredericosantos0309@gmail.com>
2026-01-19 18:43:45 -0300

    Create presents.cpp

---
+++ A2OJ/Ladder4_Codeforces_Div2A/9_Presents/presents.cpp
@@ -0,0 +1,30 @@
+/*
+ * Autor: Frederico S. Gonçalves e Bernardo Drummond
+ * Data: 19/01/2026
+ *
+ * Plataforma: A2OJ
+ * Problema: Presents
+ * Link: https://codeforces.com/problemset/problem/136/A
+ * Dificuldade: 2
+ */
+
+#include <bits/stdc++.h>
+#define SUCESSO (0)
+#define TAM_MAX (100)
+#define pb push_back
+using namespace std;
+
+int main() {
+  int qntParticipants;
+  cin >> qntParticipants;
+
+  int presents[TAM_MAX];
+  for(int i = 1; i <= qntParticipants; i++) {
+    int v;
+    cin >> v;
+    presents[v] = i;
+  }
+
+  for(int i = 1; i <= qntParticipants; i++) cout << presents[i] << ' ';
+  return SUCESSO;
+}
