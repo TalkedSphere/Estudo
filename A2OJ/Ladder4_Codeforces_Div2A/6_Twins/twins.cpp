3c70ad54abb54b207fa8a4323fabc2f2709af820
Frederico S. Gonçalves <fredericosantos0309@gmail.com>
2026-01-19 17:29:45 -0300

    Create twins.cpp

---
+++ A2OJ/Ladder4_Codeforces_Div2A/6_Twins/twins.cpp
@@ -0,0 +1,42 @@
+/*
+ * Autor: Frederico S. Gonçalves e Bernardo Drummond
+ * Data: 19/01/2026
+ *
+ * Plataforma: A2OJ
+ * Problema: Twins
+ * Link: https://codeforces.com/problemset/problem/160/A
+ * Dificuldade: 2
+ */
+
+#include <bits/stdc++.h>
+#define pb push_back
+using namespace std;
+
+int n;
+vector<int> c;
+int ans = 1;
+
+int main(void) {
+  cin >> n;
+
+  for(int i = 0; i < n; i++) {
+    int v;
+    cin >> v;
+    c.pb(v);
+  }
+
+  sort(c.begin(), c.end());
+
+  for(int i = n - 1; i >= 0; i--) {
+    int sumEsq = 0, sumDir = 0;
+
+    for(int j = 0; j < i; j++) sumEsq += c[j];
+    for(int k = i; k < n; k++) sumDir += c[k];
+
+    if(sumEsq >= sumDir) ans++;
+    else break;
+  }
+
+  cout << ans;
+  return 0;
+}
