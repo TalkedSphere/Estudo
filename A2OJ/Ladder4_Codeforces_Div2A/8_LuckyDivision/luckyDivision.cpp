73151a9d942bed02a054a1519b376d5f683271e8
Frederico S. Gonçalves <fredericosantos0309@gmail.com>
2026-01-19 18:06:42 -0300

    Create luckyDivision.cpp

---
+++ A2OJ/Ladder4_Codeforces_Div2A/8_LuckyDivision/luckyDivision.cpp
@@ -0,0 +1,38 @@
+/*
+ * Autor: Frederico S. Gonçalves e Bernardo Drummond
+ * Data: 19/01/2026
+ *
+ * Plataforma: A2OJ
+ * Problema: Twins
+ * Link: https://codeforces.com/problemset/problem/122/A
+ * Dificuldade: 2
+ */
+
+#include <bits/stdc++.h>
+#define SUCESSO 0
+using namespace std;
+const vector<int> luckyNums = {4, 7};
+
+int main() {
+  int num;
+  cin >> num;
+
+  int c = 0, d = 0, u = 0;
+  for(int i = 0; i < luckyNums.size(); i++) {
+    for(int j = 0; j < luckyNums.size(); j++) {
+      for(int k = 0; k < luckyNums.size(); k++) {
+        u = luckyNums[k];
+        int divisor = (c*100) + (d*10) + u;
+        if(num % divisor == 0) {
+          cout << "YES";
+          return SUCESSO;
+        }
+      }
+      d = luckyNums[j];
+    }
+    c = luckyNums[i];
+  }
+
+  cout << "NO";
+  return SUCESSO;
+}
