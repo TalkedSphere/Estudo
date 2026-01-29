2fd875bb920b392f1df804dc7b114b1f683f2dbd
Frederico S. Gonçalves <fredericosantos0309@gmail.com>
2026-01-29 15:53:59 -0300

    Create xeniaAndRigroad.cpp

---
+++ A2OJ/Ladder5_Codeforces_Div2B/002_XeniaAndRingroad/xeniaAndRigroad.cpp
@@ -0,0 +1,33 @@
+/*
+ * Autor: Frederico Santos Gonçalves
+ * Data: 29/01/2026
+ *
+ * Plataforma: A2OJ
+ * Problema: Xenia and Ringroad
+ * Link: https://codeforces.com/problemset/problem/339/B
+ * Dificuldade: 2
+ */
+
+#include <bits/stdc++.h>
+#define SUCESSO (0)
+using namespace std;
+ 
+int main() {
+  int n, m;
+  int pos = 1, nextPos, time = 0;
+
+  cin >> n >> m;
+
+  for(int i = 0; i < m; i++) {
+    cin >> nextPos;
+    if(nextPos >= pos) time += nextPos-pos;
+    else {
+      time += n-pos;
+      time += nextPos;
+    }
+    pos = nextPos;
+  }
+
+  cout << time;
+  return SUCESSO;
+}
